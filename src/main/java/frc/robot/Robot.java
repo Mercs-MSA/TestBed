0e89669ccc7980b266f9bf981e2e09b33aaba18e
Gaurav Salandri <salangau000@gmail.com>
2024-05-15 19:37:07 -0500

    limelight result validation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,16 +6,11 @@ package frc.robot;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.networktables.DoubleArrayPublisher;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.LimelightHelpers.LimelightResults;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
@@ -36,11 +31,11 @@ public class Robot extends TimedRobot {
     LimelightHelpers.SetRobotOrientation("limelight", pigeon2.getAngle(), 0, 0, 0, 0, 0);
 
     var lastResult = LimelightHelpers.getLatestResults("limelight");
-    if (lastResult.botpose_tagcount > 0) {
+    if (lastResult.valid) {
     m_robotContainer.drivetrain.addVisionMeasurement(LimelightHelpers.getBotPose2d_wpiBlue("limelight"), Timer.getFPGATimestamp());
-      SmartDashboard.putString("lltags", lastResult.error);
+      SmartDashboard.putBoolean("limelightResultValid", true);
     } else {
-      SmartDashboard.putString("lltags", lastResult.error);
+      SmartDashboard.putBoolean("limelightResultValid", false);
     }
   }
 
